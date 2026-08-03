481fcade99f74986fcd774ab515ca5e4b99854f1
wisdomsunday750 <wsunday344@gmail.com>
2026-08-03 15:35:54 +0100

    Refactor Benz Garage system for car management

--- Benz-Garage.py
+++ Benz-Garage.py
@@ -1,225 +1,193 @@
-def get_number(promt):
-  """ keep asking until the user type a valid number. No crashes """
-  while True:
-    try:
-      number = int(input(promt))
+import datetime
+import csv
+def get_number(prompt):
+  """keep asking till user type valid number"""
+  while True :
+    try :
+      number = int(input(prompt))
       if number < 0:
-        print("Number cannot be negative! try again")
-        continue
+       print("Number cannot be Negative! try again")
+       continue
       return number
     except:
       print("Invalid! please enter numbers only")
-def get_text(promt):
+def get_text(prompt):
   while True:
-    text = input(promt).lower().strip()
-    if text == "":
-      print("cannot be empty! try again")
-    else:
-      return text
-import datetime
-def export_to_csv(students):
-  """ export all students data to csv file that can be opened in Excel"""
-  file = open("report.csv", "w")
-  file.write("Name, Age, City, Laptop_money \n")
-  for name, data in students.items():
-    file.write(name + "," + str(data["age"]) + "," + data["city"] + "," + str(data["laptop_money"]) + "\n")
-  file.close()
-  print("Report exported to report.csv✅ open it with Excel!")
-def backup_data(students):
-  """ create a backup file with today's date so data is never lost"""
-  today = datetime.datetime.now().strftime("%y-%m-%d") # Gets today's date like 23-08-2026
-  backup_filename = "Benz_garage_backup_" + today + ".txt"
-  file = open(backup_filename, "w")
-  for name, data in students.items():
-    file.write(name + "," + str(data["age"]) + "," + data["city"] + "," + str(data["laptop_money"]) + "\n")
-  file.close()
-  print("backup saved as:", backup_filename, "✅")
-def load_different_file():
-  filename = input("Enter filename to load: ")
-  students = {}
-  try:
-    file = open(filename, "r")
-    for line in file:
-      name, age, city, money = line.strip().split(",")
-      students[name]= {"age": int(age), "city": city, "laptop_money": int(money)}
-    file.close()
-    print("loaded", len(students), "students from", filename, "✅")
-  except:
-    print("File not found! check spelling")
-  return students
+    try:
+      text = input(prompt).lower().strip()
+      if text =="":
+        print("cannot be empty! try again")
+      else:
+        return text
+    except: 
+      print("invalid! please enter text only")
 def load_data():
-  students = {}
+  cars = {}
   try:
-    file = open("Benz_garage.txt", "r")
+    file = open("Benz.garage.csv", "r")
     for line in file:
-      name, age, city, money = line.strip().split(",")
-      students[name]= {"age": int(age), "city": city, "laptop_money": int(money)}
+      model, year, color, price, qty= line.strip().split(",")
+      cars[model] = {"year": int(year), "color": color, "price": int(price), "quantity": int(qty)}
     file.close()
-    print("loaded", len(students), "students from file✅")
+    print("loaded", len(cars), "cars from file")
   except:
-    print("no saved file found. starting fresh!")
-  return students
-def save_data(students):
-  file = open("Benz_garage.txt", "w")
-  for name, data in students.items():
-    file.write(name + "," + str(data["age"]) + "," + data["city"] + "," + str(data["laptop_money"]) + "\n")
+    print("no saved file found, starting from fresh!")
+  return cars 
+def save_data(cars):
+  file = open("Benz.garage.csv", "w")
+  for model, data in cars.items():
+    file.write(model + "," + str(data["year"]) + "," + data["color"] + "," + str(data["price"]) + "," + str(data["quantity"]) + "\n")
   file.close()
-def show_all_students(students):
-  print("\n--- ALL STUDENT---")
-  print("Name".ljust(10), "Age".ljust(5), "City".ljust(10), "Money")
-  print("-"*40)
-  for name, data in students.items():
-    print(name.ljust(10),str(data["age"]).ljust(5), data["city"].ljust(10),data["laptop_money"])
-students = load_data()
-def search_by_city(students):
-  target_city = input("Enter city to search: ").lower().strip()
+def garage_report(cars):
+  if len(cars) ==0:
+    print("no cars yet")
+    return
+  total_value = 0
+  total_cars = 0
+  most_expensive = 0
+  for model, data in cars.items():
+    car_value = data["price"]* data["quantity"]
+    total_value += car_value
+    total_cars += data["quantity"]
+    if data ["price"] > most_expensive:
+      most_expensive = data["price"]
+      model_expensive = model
+  print("\n--- BENZ GARAGE REPORT---")
+  print("total cars models:", len(cars))
+  print("total cars in stock:", total_cars)
+  print("total inventory value: $", total_value)
+  print("most expensive car:", model_expensive, " - $", most_expensive)
+def show_all_cars(cars):
+  print("\n---- ALL CARS----")
+  print("Model". ljust(15), "year". ljust(6), "color". ljust(10), "price". ljust(10), "Qty")
+  print("-"*60)
+  for model, data in cars.items():
+    print(model.ljust(15),str(data["year"]).ljust(6), data["color"].ljust(10), ("$" + str(data["price"])).ljust(10), data["quantity"])
+cars = load_data()
+def search_by_color(cars):
+  target_color = get_text("enter color to search: ").lower().strip()
   found = False
-  print("\n---STUDENTS IN", target_city.upper(), "---")
-  for name, data in students.items():
-    if data["city"]== target_city:
-      print(name.ljust(10), str(data["age"]).ljust(5), data["city"].ljust(10), data["laptop_money"])
+  print("\n---CARS WITH COLOR---", target_color.upper(), "---")
+  for model, data in cars.items():
+    if data["color"].lower().strip() == target_color:
+      print(model.ljust(15), str(data["year"]).ljust(15), data["color"].ljust(10), ("$" + str(data["price"])).ljust(10), data["quantity"])
       found = True
-  if found ==False:
-    print("No student found in", target_city)
-def sort_by_money(students):
-  print("\n---STUDENTS SORTED BY MONEY RICHEST FIRST---")
-  # sort students by money. x[1] is the data. Reverse=True  = biggest first
-  sorted_students = sorted(students.items(), key=lambda x: x[1]["laptop_money"], reverse= True)
-  print("NAME".ljust(10), "AGE".ljust(5), "CITY".ljust(10), "MONEY")
-  print("-"*40)
-  for name, data in sorted_students:
-    print(name.ljust(10), str(data["age"]).ljust(5), data["city"].ljust(10), data["laptop_money"])
+  if found == False:
+    print("No cars found with color:", target_color)
+def export_to_csv(cars):
+  filename = "Benz_garage_report.csv"
+  with open(filename, "w", newline="") as file:
+    writer = csv.writer(file)
+    writer.writerow(["model", "year", "color", "price", "quantity"])
+    for model, data in cars.items():
+      writer.writerow([model, data["year"], data["color"], data["price"], data["quantity"]])
+  print("Report expected to", filename, "✅open it with excel")
 while True:
-  print("\n==== Benz Garage System====")
-  print("1. Report")
-  print("2. search student")
-  print("3. Add student")
-  print("4. Delete student")
-  print("5. Update student")
-  print("6. Show_all_students")
-  print("7. Search by City")
-  print("8. Sort by Money")
-  print("9. Save & Exit")
-  print("10. Backup Data")
-  print("11. Load Different Data")
-  print("12. Export to Excel/Csv")
-  print("13. Garage Statistics")
-  choice = input("Enter 1-13: ")
+  print("1. Add Car")
+  print("2. Show All Cars")
+  print("3. Delete Car")
+  print("4. Update Car")
+  print("5. Search by Car")
+  print("6. Search By Color")
+  print("7. Garage Report")
+  print("8. Export For Excel (Csv)")
+  print("9.  Save & Exit")
+  choice = input("Enter choice 1-9: ")
   if choice == "1":
-    if len(students) == 0:
-      print("no students yet!")
-    else:
-      richest_name = ""
-      richest_money = 0
-      poorest_name = ""
-      poorest_money = float('inf')
-      total_money = 0
-      count = 0
-      for name, data in students.items():
-        if data["laptop_money"] > richest_money:
-          richest_money = data["laptop_money"]
-          richest_name = name
-        if data["laptop_money"] < poorest_money:
-          poorest_money = data["laptop_money"]
-          poorest_name = name
-        total_money = total_money + data["laptop_money"]
-        count = count + 1
-      average = total_money / count
-      print("---Benz Garage Report---")
-      print("RICHEST:", richest_name, richest_money, "naira")
-      print("POOREST:", poorest_name, poorest_money, "naira")
-      print("AVERAGE:", round(average, 2), "naira")
+    model = get_text("Enter car model:")
+    year = get_number("Enter  year:")
+    color = get_text("Enter color:")
+    price = get_number("Enter price")
+    qty = get_number("Enter quantity")
+    cars[model] = {"year": year, "color": color, "price": price, "quantity": qty}
+    save_data(cars)
+    print(model, "Added")
   elif choice == "2":
-    search_name = input("who do you want to search? ").lower().strip()
-    if search_name in students:
-      person = students[search_name]
-      print("---FOUND---")
-      print("Name:", search_name, "Age:", person["age"], "City:", person["city"], "Money:", person["laptop_money"])
-    else:
-      print("sorry,", search_name, "is not in Benz garage yet!")
+    show_all_cars(cars)
+    if len(cars) == 0:
+      print("No cars in garage")
   elif choice == "3":
-    new_name = get_text("Enter new name: ").lower().strip()
-    if new_name in students:
-      print(new_name, "is already in Benz garage!")
+    delete_car = input("which car do you want to delete? ").lower().strip()
+    if delete_car in cars:
+      del cars[delete_car]
+      save_data(cars)
+      print(delete_car, "has been removed from Benz garage! ")
     else:
-      new_age = get_number("Enter new age: ")
-      new_city = get_text("Enter new city: ").lower().strip()
-      money = get_number("Enter new money: ")
-      students[new_name] = {"age": new_age, "city": new_city, "laptop_money": money}
-      save_data(students)
-      print(new_name, "has been added to Benz garage! ")
+      print("sorry,", delete_car, "is not in the Benz garage! ")
   elif choice == "4":
-    delete_student = input("who do you want to delete? ").lower().strip()
-    if delete_student in students:
-      del students[delete_student]
-      print(delete_student, "has been removed from Benz garage! ")
-    else:
-      print("sorry,", delete_student, "is not in the Benz garage! ")
-  elif choice == "5":
-    update_name = get_text("who do you want to update? ").lower().strip()
-    if update_name in students:
-      print("1. Age 2. City 3. Money 4. All")
-      sub_choice = input("1, 2, 3, or 4:")
+    update_car = get_text("which car do you want to update? ").lower().strip()
+    if update_car in cars:
+      print("1. model 2. year 3. color 4. price 5. quantity 6. All")
+      sub_choice = input("1, 2, 3, 4, 5 or 6:")
       if sub_choice == "1":
         try:
-         students[update_name]["age"]= get_number("Enter new age: ")
-         save_data(students)
-         print("age updated!")
+          new_model = get_text("enter new model:")
+          cars[new_model]= cars.pop(update_car)
+          save_data(cars)
+          print("model updated")
         except:
-          print("invalid! Age must be numbers")
+          print("Invalid! model must be text")
       elif sub_choice == "2":
-        students[update_name]["city"]= get_text("Enter new city: ").lower().strip()
-        print("city updated!")
+        try:
+          cars[update_car]["year"]= get_number("enter new year:")
+          save_data(cars)
+          print("year updated")
+        except:
+          print("Invalid! year must be numbers")
       elif sub_choice == "3":
         try:
-         students[update_name]["laptop_money"]= get_number("Enter new laptop_money: ")
-         print("laptop_money updated!")
+          cars[update_car]["color"]= get_text("enter new color:")
+          save_data(cars)
+          print("color updated")
         except:
-          print("invalid! Money must be numbers")
+          print("Invalid! color must be text")
       elif sub_choice == "4":
         try:
-         students[update_name]["age"]= get_number("Enter new age: ")
-         students[update_name]["city"]= get_text("Enter new city: ").lower().strip()
-         students[update_name]["laptop_money"]= get_number("Enter new laptop_money: ")
-         save_data(students)
-         print("All details updated!")
+          cars[update_car]["price"]= get_number("enter new price:")
+          save_data(cars)
+          print("price updated")
         except:
-          print("invalid! Age and Money must be numbers")
-      else:
-        print("invalid choice")
+          print("Invalid! price must be number")
+      elif sub_choice == "5":
+        try:
+         cars[update_car]["quantity"]= get_number("enter new quantity:")
+         save_data(cars)
+         print("quantity updated")
+        except:
+          print("invalid! quantity must be number")
+      elif sub_choice == "6":
+        try:
+          new_model = get_text("enter new model: ")
+          cars[new_model]= cars.pop(update_car)
+          cars[new_model]["year"]= get_number("enter new year:")
+          cars[new_model]["color"]= get_text("enter new color:")
+          cars[new_model]["price"]= get_number("enter new price:")
+          cars[new_model]["quantity"]= get_number("enter new quantity:")
+          save_data(cars)
+          print("All details updated!")
+        except:
+          print("invalid! year, price and quantity must be numbers.")
     else:
-      print("sorry,", update_name, "is not in the Benz garage!")
+        print("sorry,", update_car, "is not in the Benz garage!")
+  elif choice == "5":
+    search = get_text("search car model: ")
+    if search in cars:
+      data = cars[search]
+      print("---FOUND---")
+      print("model:", search, "year:", data["year"], "color:", data["color"], "price: $", data["price"], "Qty:", data ["quantity"])
+    else:
+      print("sorry,", search, "is not in Benz garage yet!")
   elif choice == "6":
-    show_all_students(students)
+    search_by_color(cars)
   elif choice == "7":
-    search_by_city(students)
+    garage_report (cars)
   elif choice == "8":
-    sort_by_money(students)
-  elif choice == "10":
-    backup_data(students)
-  elif choice == "11":
-    students = load_different_file()
-  elif choice == "12":
-    export_to_csv(students)
-  elif choice == "13":
-    if len(students) == 0:
-      print("No students yet!")
-    else:
-      total_students = len(students)
-      total_money = 0
-      total_age = 0
-      for name, data in students.items():
-        total_money = total_money + data["laptop_money"]
-        total_age = total_age + data["age"]
-      Average_age = total_age / total_students
-      print("\n---GARAGE STATISTICS---")
-      print("Total Students:", total_students)
-      print("Total Money In Garage:", total_money, "naira")
-      print("Average Age:", round(Average_age, 1))
-      print("----------\n")
+    export_to_csv (cars)
   elif choice == "9":
-    save_data(students)
-    print("data saved! thank you for using Benz Garage! bye👋")
+    save_data(cars)
+    print("data saved! thank you for using Benz Barage bye👋")
     break
   else:
-    print("invalid choice, Enter 1-13")
+    print("invalid choice, enter 1-9")
+       
+     
